642d2bac87216c46e31aee30e6e2456a1ce1e4a6
paulnbrd <paulin.burbaud@gmail.com>
2022-07-11 17:20:39 +0200

    Changed tempfile mode

--- dev/youtube/module.py
+++ dev/youtube/module.py
@@ -1,6 +1,4 @@
-import tempfile
 from yt_dlp import YoutubeDL
-from yt_dlp import utils as ytutils
 import shutil
 import utile.utils
 import os
@@ -60,69 +58,69 @@ def execute(*urls, onlyaudio: bool = False):
             return
     for url in urls:
         try:
-            with tempfile.NamedTemporaryFile(delete=False) as temp:
-                temp.close()  # Because youtube_dl needs to access it
-                file_destination = temp.name
-                try:
-                    with utile.utils.create_spinner() as context:
-                        def hook(data: dict):
-                            if data.get("status") == "downloading":
-                                context.text = "ETA: {}, {} done".format(
-                                    data["_eta_str"], data["_percent_str"])
-                            elif data.get("status") == "finished":
-                                context.text = "Finished{}".format(
-                                    '. Converting to audio...' if audio_only else ''
-                                )
-                            else:
-                                context.text = data.get("status")
-                        opts = {
-                            "quiet": True,
-                            "logger": yt_logger,
-                            "progress_hooks": [hook],
-                            "outtmpl": file_destination,
-                            'format': 'bestaudio/best' if audio_only else None,
-                        }
-                        if audio_only:
-                            opts["postprocessors"] = [{
-                                'key': 'FFmpegExtractAudio',
-                                'preferredcodec': 'mp3',
-                                'preferredquality': '192'
-                            }]
-                        ydl = YoutubeDL(opts)
-                        with ydl:
-                            context.text = "Extracting video infos from url {}...".format(
-                                termcolor.colored(url, "green")
+            random_name = os.urandom(8).hex()
+            random_name_path = utile.utils.Directory.get_cache_path("youtube", random_name)
+            file_destination = random_name_path
+            try:
+                with utile.utils.create_spinner() as context:
+                    def hook(data: dict):
+                        if data.get("status") == "downloading":
+                            context.text = "ETA: {}, {} done".format(
+                                data["_eta_str"], data["_percent_str"])
+                        elif data.get("status") == "finished":
+                            context.text = "Finished{}".format(
+                                '. Converting to audio...' if audio_only else ''
                             )
-                            infos = ydl.extract_info(url, download=False)
-                            context.text = "Downloading video '{}'...".format(
-                                termcolor.colored(infos["title"], "green"))
-                            ydl.download([url])
-                            context.text = "Resolving filepath..."
-                            if audio_only:
-                                file_destination = os.path.join(
-                                    os.path.dirname(file_destination), ".mp3")
-                                ext = "mp3"
-                            else:
-                                ext = infos["ext"]
+                        else:
+                            context.text = data.get("status")
+                    opts = {
+                        "quiet": True,
+                        "logger": yt_logger,
+                        "progress_hooks": [hook],
+                        "outtmpl": file_destination,
+                        'format': 'bestaudio/best' if audio_only else None,
+                    }
+                    if audio_only:
+                        opts["postprocessors"] = [{
+                            'key': 'FFmpegExtractAudio',
+                            'preferredcodec': 'mp3',
+                            'preferredquality': '192'
+                        }]
+                    ydl = YoutubeDL(opts)
+                    with ydl:
+                        context.text = "Extracting video infos from url {}...".format(
+                            termcolor.colored(url, "green")
+                        )
+                        infos = ydl.extract_info(url, download=False)
+                        context.text = "Downloading video '{}'...".format(
+                            termcolor.colored(infos["title"], "green"))
+                        ydl.download([url])
+                        context.text = "Resolving filepath..."
+                        if audio_only:
+                            file_destination = os.path.join(
+                                os.path.dirname(file_destination), ".mp3")
+                            ext = "mp3"
+                        else:
+                            ext = infos["ext"]
 
-                            new_path = utile.utils.Directory.YOUTUBE_VIDEOS + os.sep + "".join([
-                                c for c in infos["title"]
-                                if c.isalpha() or c.isdigit() or c == ' ']).rstrip() + "." + ext
-                            i = 1
-                            while os.path.isfile(new_path):
-                                new_path = utile.utils.Directory.YOUTUBE_VIDEOS + os.sep + "({}) ".format(i) + \
-                                    "".join([
-                                        c for c in infos["title"]
-                                        if c.isalpha() or c.isdigit() or c == ' ']).rstrip() + "." + ext
-                                i += 1
-                            context.text = "Moving file..."
-                            shutil.move(file_destination, new_path)
-                            os.unlink(file_destination)
-                            context.text = termcolor.colored("Done", "green")
-                    print("\nDownloaded {}".format(
-                        termcolor.colored(infos["title"], "green")))
-                except Exception as e:
-                    print("Could not download video (url: {}, error: {})".format(url, e))
+                        new_path = utile.utils.Directory.YOUTUBE_VIDEOS + os.sep + "".join([
+                            c for c in infos["title"]
+                            if c.isalpha() or c.isdigit() or c == ' ']).rstrip() + "." + ext
+                        i = 1
+                        while os.path.isfile(new_path):
+                            new_path = utile.utils.Directory.YOUTUBE_VIDEOS + os.sep + "({}) ".format(i) + \
+                                "".join([
+                                    c for c in infos["title"]
+                                    if c.isalpha() or c.isdigit() or c == ' ']).rstrip() + "." + ext
+                            i += 1
+                        context.text = "Moving file..."
+                        shutil.move(file_destination, new_path)
+                        os.unlink(file_destination)
+                        context.text = termcolor.colored("Done", "green")
+                print("\nDownloaded {}".format(
+                    termcolor.colored(infos["title"], "green")))
+            except Exception as e:
+                print("Could not download video (url: {}, error: {})".format(url, e))
         except PermissionError:
             print("The script is missing permissions to write temporary files")
 
